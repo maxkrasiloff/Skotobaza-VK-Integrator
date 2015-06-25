82435e6651ce51d2045cfa8195d091eb74edd5ba
maxkrasiloff <nashaarmia@yandex.ru>
2015-06-25 20:15:49 +0500

    skotobaza.user.js

---
+++ skotobaza.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Skotobaza
+// @namespace    http://bitardus.blogspot.ru/
+// @version      0.1
+// @description  enter something useful
+// @author       max_krasiloff
+// @match        https://vk.com/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-1.11.3.min.js
+// ==/UserScript==
+
+//var URLa = document.location.href;
+var URLa = $("#page_wall_header").attr("href");
+URLa = URLa+"";
+URLa = URLa.replace('/wall', 'http://skotobaza.org/user/id');
+$("#profile_short").append('<a href="'+URLa+'" target="_blank"><div style="text-align: center; background-color: #ddddff; color: #000; border: 1px solid #aaaaff; padding: 5px; font-size: 18px;">Ссыкла на скотобазу</div></a>');
+//alert(URLa);
+//$("body").css("background-color","#eee");
