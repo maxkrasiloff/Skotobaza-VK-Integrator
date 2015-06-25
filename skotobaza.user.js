1beecbebfbf5a53f16b6c6e87348766a1b1716c3
maxkrasiloff <nashaarmia@yandex.ru>
2015-06-25 20:19:09 +0500

    Update skotobaza.user.js

--- skotobaza.user.js
+++ skotobaza.user.js
@@ -9,10 +9,9 @@
 // @require      http://code.jquery.com/jquery-1.11.3.min.js
 // ==/UserScript==
 
-//var URLa = document.location.href;
 var URLa = $("#page_wall_header").attr("href");
 URLa = URLa+"";
 URLa = URLa.replace('/wall', 'http://skotobaza.org/user/id');
 $("#profile_short").append('<a href="'+URLa+'" target="_blank"><div style="text-align: center; background-color: #ddddff; color: #000; border: 1px solid #aaaaff; padding: 5px; font-size: 18px;">Ссыкла на скотобазу</div></a>');
-//alert(URLa);
-//$("body").css("background-color","#eee");
+
+
